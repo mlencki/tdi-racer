adcfbf57f033028347a545287bd19a1ed52dc048
Mateusz Lencki <mateusz.lencki@gmail.com>
2020-12-30 21:15:51 +0100

    Update GameStateManager.cs

--- Assets/Scripts/GameStateManager.cs
+++ Assets/Scripts/GameStateManager.cs
@@ -7,7 +7,7 @@ public class GameStateManager: MonoBehaviour
     public bool gameOver = false;
     public float gameSpeed;
 
-	public void Start ()
+    public void Start ()
     {
         this.gameSpeed = 3f;
         StartCoroutine("IncreaseGameSpeed");
